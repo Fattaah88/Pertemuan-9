ca0a7ca01a1b6dc9141f4ad17b2d873f9cd2ee79
Fattaah88 <fattaahherdansyah@students.amikom.ac.id>
2023-06-15 14:18:37 +0700

    Upload Source Code

---
+++ Printer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pertemuan_9
+{
+    class Printer
+    {
+        public virtual void DisplayDimension()
+        {
+            Console.WriteLine("Default display dimension");
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Default Printing");
+        }
+    }
+
+    class EpsonPrinter : Printer
+    {
+        public override void DisplayDimension()
+        {
+            Console.WriteLine("Epson display dimension : 10*11");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Epson printer printing ...");
+        }
+    }
+
+    class CanonPrinter : Printer
+    {
+        public override void DisplayDimension()
+        {
+            Console.WriteLine("Canon display dimension : 9.5*12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Canon printer printing ...");
+        }
+    }
+
+    class LaserJetPrinter : Printer
+    {
+        public override void DisplayDimension()
+        {
+            Console.WriteLine("LaserJet display dimension : 12*12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("LaserJet printer printing ...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pertemuan_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Silahkan Pilih Printer : ");
+            Console.WriteLine("1.Epson");
+            Console.WriteLine("2.Canon");
+            Console.WriteLine("3.LaserJet");
+            Console.WriteLine("Masukan Angka Pilihan Printer Anda : "); int input = Convert.ToInt32(Console.ReadLine());
+
+            Printer printer;
+
+            if(input == 1)
+            {
+                printer = new EpsonPrinter();
+            }
+            else if (input == 2)
+            {
+                printer = new CanonPrinter();
+            }
+            else if (input == 3)
+            {
+                printer = new LaserJetPrinter();
+            }
+            else
+            {
+                Console.WriteLine("Pilihan Printer Anda Salah");
+                return;
+            }
+
+            printer.DisplayDimension();
+            printer.Print();
+        }
+    }
+}
